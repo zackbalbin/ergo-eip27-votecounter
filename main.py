7f48d47dcd8dadc5b356531cb24cc1fa52b3a396
Zack Balbin <zack@zackbalbin.com>
2022-05-18 21:49:17 -0700

    update to track per epoch

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import requests
 import os
 
-blocksToScan = 500
-
 os.system('cls' if os.name == 'nt' else 'clear')
 
 print("Ergo Mainnet EIP27 Vote Count")
 
 url = "https://api.ergoplatform.com/api/v1/blocks?limit=1"
-heightData = requests.get(url).json()["total"]
+chainData = requests.get(url).json()
+height = chainData["total"]
+epoch = chainData["items"][0]["epoch"]
+blocksInEpoch = height % 1024
+blocksUntilNewEpoch = 1024 - blocksInEpoch
 
-print("Chain Height:", heightData)
-print("Blocks To Scan:", blocksToScan)
-
-url = "https://api.ergoplatform.com/api/v1/blocks/?limit=" + str(blocksToScan) + "&offset=0"
-blockData = requests.get(url).json()["items"]
+print("Chain Height:", height)
+print("Epoch Number:", epoch)
+print("Blocks In Epoch:", blocksInEpoch)
+print("Blocks Until New Epoch:", blocksUntilNewEpoch)
 
 votesYes = 0
 votesNo = 0
 
-print("\nVote Data")
-for i in blockData:
-    id = i["id"]
-    url = "https://api.ergoplatform.com/api/v1/blocks/" + id
-    blockByIdData = requests.get(url).json()
-    vote = blockByIdData["block"]["header"]["votes"]
-    if vote[0] == 8:
-        votesYes += 1
+while (blocksInEpoch > 0):
+    url = ""
+    if blocksInEpoch >= 500:
+        url = "https://api.ergoplatform.com/api/v1/blocks/?limit=500&offset=0"
+        blocksInEpoch -= 500
     else:
-        votesNo += 1
+        url = "https://api.ergoplatform.com/api/v1/blocks/?limit=" + str(blocksInEpoch) + "&offset=0"
+        blocksInEpoch -= blocksInEpoch
+    blockData = requests.get(url).json()["items"]
+    for i in blockData:
+        id = i["id"]
+        url = "https://api.ergoplatform.com/api/v1/blocks/" + id
+        blockByIdData = requests.get(url).json()
+        vote = blockByIdData["block"]["header"]["votes"]
+        if vote[0] == 8:
+            votesYes += 1
+        else:
+            votesNo += 1
 
+
+print("\nVote Data")
 print("Votes Yes:", votesYes)
 print("Votes No:", votesNo)
 print("Percent Yes: " + str((votesYes / (votesYes + votesNo) * 100)) + "%")
